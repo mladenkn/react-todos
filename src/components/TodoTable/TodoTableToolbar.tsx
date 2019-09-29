bae252ebf705860059e9ccec71a01ac9e6db310c
Mladen <mladsdf@sdf.com>
2019-09-29 14:23:56 +0200

    updated TodoTableToolbar css

--- src/components/TodoTable/TodoTableToolbar.tsx
+++ src/components/TodoTable/TodoTableToolbar.tsx
@@ -1,43 +1,9 @@
 import { makeStyles } from "@material-ui/styles";
-import { Tooltip, Typography, Toolbar, IconButton, TextField, Theme, createStyles } from "@material-ui/core";
-import React, { Fragment } from "react";
-import clsx from "clsx";
+import { Tooltip, Typography, IconButton, TextField, Theme, createStyles } from "@material-ui/core";
+import React from "react";
 import DeleteIcon from '@material-ui/icons/Delete';
 import PostAddIcon from '@material-ui/icons/PostAdd';
 import { lighten } from "@material-ui/core/styles";
-
-const useToolbarStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(1),
-      marginBottom: '0.5em',
-      paddingTop: '0.5em',
-      height: '5em',
-    },
-    highlight:{
-      color: theme.palette.secondary.main,
-      backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-    },
-    actions: {
-      marginLeft: '13em',      
-    },
-    titleContainer: {
-    },
-    selectedCount: {
-      fontSize: '2em',
-    },
-    title: {
-      fontSize: '3em',
-    },
-    searchField: {
-      width: '12em',
-    },
-    deleteAction: {
-      marginLeft: '6.4em',
-    },
-  }), { name: 'Toolbar'},
-);
 
 interface EnhancedTableToolbarProps {
   numSelected: number
@@ -47,49 +13,88 @@ interface EnhancedTableToolbarProps {
   onTodoAddClick: () => void
 }
 
-export const TodoTableToolbar = (props: EnhancedTableToolbarProps) => {
-  const classes = useToolbarStyles();
-  const { numSelected } = props;
+const sharedStyles = createStyles({
+  root: {
+    height: '6em',
+    boxSizing: 'border-box',
+  },
+})
+
+const useRootSelectedStyles = makeStyles((theme: Theme) => ({
+  root: {
+    color: theme.palette.secondary.main,
+    backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+    display: 'flex',
+    alignItems: 'center',
+    ...sharedStyles.root,    
+  },
+  title: {
+    fontSize: '2em',
+    marginLeft: '0.8em',
+  },
+  deleteAction: {
+    marginLeft: '60.5%'
+  },
+}))
+
+const useRootNormalStyles = makeStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'initial',
+    padding: '1em 0em 0.7em 1.5em',
+    ...sharedStyles.root,
+  },
+  title: {
+    fontSize: '2.8em',
+  },
+  searchField: {
+    marginLeft: '22%',
+    marginTop: '0.2em',
+    width: '15em',
+  },
+  addTodoAction: {
+    marginLeft: '1em',
+    padding: '0.8em',
+  },
+})
+
+export const TodoTableToolbar = (p: EnhancedTableToolbarProps) => {
+  const selectedClasses = useRootSelectedStyles()
+  const normalClasses = useRootNormalStyles()
+  
+  const { numSelected } = p;
  
   return (
-    <Toolbar
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      <div className={classes.titleContainer}>
-        {numSelected > 0 ? (
-          <Typography className={classes.selectedCount} color="inherit" variant="subtitle1">
+    <div>
+      {numSelected > 0 ? (
+        <div className={selectedClasses.root}>
+          <Typography className={selectedClasses.title} color="inherit" variant="subtitle1">
             {numSelected} selected
           </Typography>
-        ) : (
-          <Typography className={classes.title} color='primary' variant="h6" id="tableTitle">
-            Todos
-          </Typography>
-        )}
-      </div>
-      <div className={classes.actions}>
-        {numSelected > 0 ? (
           <Tooltip title="Delete">
-            <IconButton className={classes.deleteAction} onClick={props.onDelete} aria-label="delete">
+            <IconButton className={selectedClasses.deleteAction} onClick={p.onDelete} aria-label="delete">
               <DeleteIcon />
             </IconButton>
           </Tooltip>
-        ) : (
-          <Fragment>
-            <TextField 
-              className={classes.searchField} 
-              label='Search' 
-              type='search' 
-              value={props.searchQuery}
-              onChange={e => props.onSearchQueryChange(e.target.value)}
-            />
-            <IconButton onClick={props.onTodoAddClick}>
-              <PostAddIcon />
-            </IconButton>
-          </Fragment>
-        )}
-      </div>
-    </Toolbar>
+        </div>
+      ) : (
+        <div className={normalClasses.root}>
+          <Typography className={normalClasses.title} color='primary' variant="h6" id="tableTitle">
+            Todos
+          </Typography>     
+          <TextField 
+            className={normalClasses.searchField} 
+            label='Search' 
+            type='search' 
+            value={p.searchQuery}
+            onChange={e => p.onSearchQueryChange(e.target.value)}
+          />
+          <IconButton className={normalClasses.addTodoAction} onClick={p.onTodoAddClick}>
+            <PostAddIcon />
+          </IconButton>       
+        </div>
+        )
+      }
+    </div>
   );
 };
